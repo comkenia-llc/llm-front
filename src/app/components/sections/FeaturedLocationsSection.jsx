1f44801331f95ade60af130fc40df5e3838796cb
Khurram Shahbaz <comkeniallc@gmail.com>
2025-10-15 08:04:22 +0500

    location section layout updated

--- src/app/components/sections/FeaturedLocationsSection.jsx
+++ src/app/components/sections/FeaturedLocationsSection.jsx
@@ -62,7 +62,7 @@ export default function FeaturedLocationsSection() {
                 </div>
 
                 {/* === Location Grid === */}
-                <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-6">
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
                     {locations.map((loc) => (
                         <LocationCard
                             key={loc.id}
